9fd971802ff0e314499978b8956d7e0b1b0ae3a1
NunesNathan <nunesxd7@hotmail.com>
2022-08-30 11:03:31 -0300

    feat: `Viagem` works

--- src/main/java/com/trybe/acc/java/planejamentodeviagem/Viagem.java
+++ src/main/java/com/trybe/acc/java/planejamentodeviagem/Viagem.java
@@ -6,13 +6,20 @@ public class Viagem {
   private String destino;
   private double distanciaKm;
   private Voo voo;
+  private Tempo t;
 
   /**
    * Método construtor da classe.
    * 
    */
-  public Viagem() {
-    /* Implemente sua solução aqui (você pode ter que alterar o construtor) */
+
+  public Viagem(String embarque, String origem, String destino, double distanciaKm) {
+    this.embarque = embarque;
+    this.origem = origem;
+    this.destino = destino;
+    this.distanciaKm = distanciaKm;
+    this.voo = new Voo();
+    this.t = new Tempo(this.embarque, this.origem, this.destino, this.retornarDuracaoVoo());
   }
 
   /**
@@ -20,14 +27,12 @@ public class Viagem {
    * 
    */
   public String retonarDesembarqueHorarioLocalDestino() {
-    Tempo tempoViagem =
-        new Tempo(this.embarque, this.origem, this.destino, this.retornarDuracaoVoo());
-    return tempoViagem.retonarDesembarqueHorarioLocalDestino();
+    return this.t.retonarDesembarqueHorarioLocalDestino();
   }
 
   // aqui consideramos que um aviao percorre em media 700 km a cada hora
   public int retornarDuracaoVoo() {
-      /* Implemente sua solução aqui */
+      return this.voo.retornarTempoVoo(this.distanciaKm);
   }
 
   /**
@@ -35,9 +40,8 @@ public class Viagem {
    * 
    */
   public String retornarInformacaoViagem() {
-    Tempo tempoViagem =
-        new Tempo(this.embarque, this.origem, this.destino, this.retornarDuracaoVoo());
-
-    /* Implemente sua solução aqui */
+    return this.voo.retornarInformacaoVoo(
+            this.embarque, this.origem, this.t.retonarDesembarqueHorarioLocalOrigem(), this.destino
+    );
   }
 }
